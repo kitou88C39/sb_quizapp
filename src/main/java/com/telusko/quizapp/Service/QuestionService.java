1c6ef1afb2aec0b4f7d9cbf411588ba056b751c2
kitou88C39 <kanfutrooper@icloud.com>
2023-09-25 09:32:01 +0900

    Service/QuestionService.javaを編集

--- src/main/java/com/telusko/quizapp/Service/QuestionService.java
+++ src/main/java/com/telusko/quizapp/Service/QuestionService.java
@@ -1,8 +1,13 @@
 package com.telusko.quizapp.Service;
 
+import com.telusko.quizapp.Question;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionService {
+
+    public List<Question> getAllQuestions() {
+
+    }
 
 }
