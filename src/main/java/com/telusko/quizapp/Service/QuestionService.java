f638ed2e551f4847b15154d7f79e57dea91661df
kitou88C39 <kanfutrooper@icloud.com>
2023-09-26 08:58:59 +0900

    Service/QuestionService.javaを編集

--- src/main/java/com/telusko/quizapp/Service/QuestionService.java
+++ src/main/java/com/telusko/quizapp/Service/QuestionService.java
@@ -1,12 +1,17 @@
 package com.telusko.quizapp.Service;
 
 import com.telusko.quizapp.Question;
+import com.telusko.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
 
     public List<Question> getAllQuestions() {
+        questionDao.getAllQuestions();
 
     }
 
