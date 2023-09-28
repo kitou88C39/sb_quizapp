6c5c16b600a27853b3cc3aa3c260089a1fbe5453
kitou88C39 <kanfutrooper@icloud.com>
2023-09-28 09:24:38 +0900

    Controller/QuestionController.javaを編集

--- src/main/java/com/telusko/quizapp/Controller/QuestionController.java
+++ src/main/java/com/telusko/quizapp/Controller/QuestionController.java
@@ -5,6 +5,7 @@ import com.telusko.quizapp.Service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,9 @@ public class QuestionController {
     @GetMapping("category/{category}")
     public List<Question> getQuestionByCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
+    }
+
+    public String addQuestion(@RequestBody Question question) {
+        questionService.addQuestion(question);
     }
 }
