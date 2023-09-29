8d7a568b9187e20250cc7d496d3388c13cdc4863
kitou88C39 <kanfutrooper@icloud.com>
2023-09-29 16:00:52 +0900

    model/Quiz.javaを編集

--- src/main/java/com/telusko/quizapp/model/Quiz.java
+++ src/main/java/com/telusko/quizapp/model/Quiz.java
@@ -3,6 +3,7 @@ package com.telusko.quizapp.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.util.List;
