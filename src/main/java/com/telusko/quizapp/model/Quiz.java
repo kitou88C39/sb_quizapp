d413ef67b73762fb5c9a690fcf05bbc3f963f5e0
kitou88C39 <kanfutrooper@icloud.com>
2023-09-29 15:58:49 +0900

    model/Quiz.javaを編集

--- src/main/java/com/telusko/quizapp/model/Quiz.java
+++ src/main/java/com/telusko/quizapp/model/Quiz.java
@@ -1,9 +1,18 @@
 package com.telusko.quizapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+
 import java.util.List;
 
+@Entity
+@Data
 public class Quiz {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private List<Question> questions;
