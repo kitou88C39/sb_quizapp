f61fc507a5d646c992fe3950b04f08beda24ac04
kitou88C39 <kanfutrooper@icloud.com>
2023-09-29 16:28:43 +0900

    Service/QuizService.javaを編集

--- src/main/java/com/telusko/quizapp/Service/QuizService.java
+++ src/main/java/com/telusko/quizapp/Service/QuizService.java
@@ -2,6 +2,7 @@ package com.telusko.quizapp.Service;
 
 import com.telusko.quizapp.dao.QuizDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +10,9 @@ public class QuizService {
 
     ＠Autowired
     QuizDao quizDao;
+
+    public ResponseEntity<String>createQuiz(String category, int numQ, String title){
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+    }
 }
